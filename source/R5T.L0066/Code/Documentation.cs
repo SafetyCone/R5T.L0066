ee200127a571b514425d7ed9054a347c40613792
davidcoats <david@getsafetycone.com>
2023-10-05 10:25:24 -0700

    Initial commit.

---
+++ source/R5T.L0066/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0066
+{
+	/// <summary>
+	/// Strict platform library for .NET Standard 2.1.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.L0066/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0066
+Strict platform library for .NET Standard 2.1.
