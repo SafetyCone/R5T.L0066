e173904bbf4f038606658e243091335b31a28998
davidcoats <david@getsafetycone.com>
2026-05-03 12:33:50 -0700

    Interim commit.

--- source/R5T.L0066/Code/Functionality/IHttpResponseMessageOperator.cs
+++ source/R5T.L0066/Code/Functionality/IHttpResponseMessageOperator.cs
@@ -11,12 +11,12 @@ namespace R5T.L0066
     public partial interface IHttpResponseMessageOperator : IFunctionalityMarker
     {
         /// <inheritdoc cref="IHttpRequestMessageOperator.Write_ToConsole(HttpRequestMessage)"/>
-        public void Write_RequestOfResponse_ToConsole(HttpResponseMessage httpResponseMessage)
+        void Write_RequestOfResponse_ToConsole(HttpResponseMessage httpResponseMessage)
             => Instances.HttpRequestMessageOperator.Write_ToConsole(
                 httpResponseMessage.RequestMessage);
 
         /// <inheritdoc cref="IHttpRequestMessageOperator.Write_ToTextWriter(HttpRequestMessage, TextWriter)"/>
-        public void Write_RequestOfResponse_ToTextWriter(
+        void Write_RequestOfResponse_ToTextWriter(
             HttpResponseMessage httpResponseMessage,
             TextWriter textWriter)
             => Instances.HttpRequestMessageOperator.Write_ToTextWriter(
